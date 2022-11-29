5dacb2fc08ef3f1d71280643f53cea42767fa4d5
Giselle <gisalina0618@gmail.com>
2022-11-29 16:02:16 -0400

    Act 3

---
+++ Prac3.js
@@ -0,0 +1,6 @@
+//Crea un programa que calcule la superficie de un círculo a partir de su radio 
+//(π * Radio2) y que después muestre el valor de dicha superficie 
+//(Nota: para calcular el cuadrado de un número puedes usar la función 
+//Math.pow(base, exponente) o bien elevar un número a una potencia 
+//utilizando dos asteriscos base**exponente).
+
