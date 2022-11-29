af318dc68037017c2ef4ea8c48729ad1b67500fa
Giselle <gisalina0618@gmail.com>
2022-11-29 15:53:59 -0400

    Prac 2

---
+++ Num2.js
@@ -0,0 +1,12 @@
+//Crea un programa que calcule la superficie de un rectángulo a 
+//partir de su base y su altura, y que después muestre el valor de dicha superficie.
+
+let base=Number(prompt("Por Favor, ingrese la base del rectángulo."))
+let altura=Number(prompt("Por Favor, ingrese la altura del rectángulo."))
+let result=base*altura;
+
+document.write("------------------------------------------------------------------------------------");
+document.write("<br>")
+document.write("La superficie del rectángulo es: ", result);
+document.write("<br>")
+document.write("------------------------------------------------------------------------------------");
