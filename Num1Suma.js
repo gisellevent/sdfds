dd5516d0550867a0b0c79ca739f814f007060ff5
Giselle <gisalina0618@gmail.com>
2022-11-29 15:36:50 -0400

    Act 1

---
+++ Num1Suma.js
@@ -0,0 +1,24 @@
+let operador=Number(prompt("Por Favor, ingrese un primer número."))
+let operando=Number(prompt("Por Favor, ingrese un segundo número."))
+let resultadosum=operador+operando;
+let resultadores=operador-operando;
+let resultadomult=operador*operando;
+let resultadodiv=operador/operando;
+
+document.write("------------------------------------------------------------------------------------");
+document.write("<br>")
+document.write("El resultado de la suma es: ", resultadosum);
+document.write("<br>")
+document.write("------------------------------------------------------------------------------------");
+document.write("<br>")
+document.write("El resultado de la resta es: ", resultadores);
+document.write("<br>")
+document.write("------------------------------------------------------------------------------------");
+document.write("<br>")
+document.write("El resultado de la multiplicación es: ", resultadomult);
+document.write("<br>")
+document.write("------------------------------------------------------------------------------------");
+document.write("<br>")
+document.write("El resultado de la división es: ", resultadodiv);
+document.write("<br>")
+document.write("------------------------------------------------------------------------------------");
